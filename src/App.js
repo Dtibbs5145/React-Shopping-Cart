b3941930e735e95681aede9c1e31172cc7a4188a
Destiny Tibbitts <dtibbs5145@gmail.com>
2019-11-15 20:59:31 -0500

    MVP hit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import { ProductContext } from './components/contexts/ProductContext';
+import { CartContext } from './components/contexts/CartContext';
 
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
-import { ProductContext } from './components/contexts/ProductContext';
-import { CartContext } from './components/contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		const addToCart = {
-			setCart: item
-		}
+			setCart([...cart, item])
 	};
 
 	return (
 
-		<ProductContext.Provider value={{ products, addItem }} className="App">
-			<CartContext.Provider value={{ cart }} >
+		<ProductContext.Provider value={[ products, addItem ]} className="App">
+			<CartContext.Provider value={[ cart ]} >
 				<Navigation cart={cart} />
 
 				{/* Routes */}
